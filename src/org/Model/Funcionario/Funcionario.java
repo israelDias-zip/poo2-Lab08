d038d78e32dd2ca0a1fc605e314d5d19ca5a470d
raeLL-zip <dev.raeldias@gmail.com>
2025-11-05 20:37:34 -0300

    Lab08

--- src/org/Model/Funcionario/Funcionario.java
+++ src/org/Model/Funcionario/Funcionario.java
@@ -1,4 +1,15 @@
 package org.Model.Funcionario;
 
-public class Funcionario {
+public abstract class Funcionario {
+    public String nome;
+    protected String matricula;
+
+    public Funcionario(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public abstract double calcularSalario();
+    public abstract void realizarTarefa();
 }
+
